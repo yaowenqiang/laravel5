8c0d8a3a0a71b1170952a804ed4c497ca969a112
yaowenqiang <yaowenqiang111@163.com>
2017-06-25 14:00:23 +0800

    add elixir and gulp demo

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,20 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    // mix.less('app.less');
+    mix.sass('app.scss').coffee();
+    mix.styles([
+        'vendor/normalize.css',
+        'app.css'
+    ],'public/output/final.css','public/css');
+    mix.scripts([
+        'vendor/jquery.js',
+        'main.js',
+        'coupon.js'
+    ],'public/output/scripts.js','public/js')
+    mix.phpUnit();
+    mix.phpSpec();
+
+    mix.version('public/css/all.css')
+
 });
