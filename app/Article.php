7745345792a2eab46abacc36168167be27852b72
yaowenqiang <yaowenqiang111@163.com>
2017-06-25 16:59:49 +0800

    add Many-To-Many demo

--- app/Article.php
+++ app/Article.php
@@ -38,4 +38,9 @@ class Article extends Model
     {
         return $this->belongsTo('App\User');
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag')->withTimestamps();
+    }
 }
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -5,5 +5,11 @@
     </h2>
     <article>
         {{ $article->body }}
+        @if($article->tags)
+            <h2>Tags:</h2>
+            @foreach($article->tags as $tag)
+            {{ $tag['name'] }}
+            @endforeach
+        @endif
     </article>
 @stop
